a431dfd9d4d4c5c4093295694597dd523ac19e64
Gabriel Ichiro <gabriel582084@gmail.com>
2023-03-24 18:04:20 -0400

    added game logic, the game its kinda playable

--- game.py
+++ game.py
@@ -1,33 +1,75 @@
 import time
 import random
 
-# Define the heuristic function
+# Define the player colors
+BLACK = 'B'
+WHITE = 'W'
 
 
-def heuristic(state):
-    # TODO: Implement your heuristic function here
-    return 0
-
-# Define the function for getting the available moves for a player
-
-
-def get_available_moves(board, player):
-    # TODO: Implement the function for getting available moves
-    return []
+def create_board():
+    board = [[None for j in range(4)] for i in range(4)]
+    board[0][0] = 'B'
+    board[0][3] = 'W'
+    board[1][1] = 'B'
+    board[1][2] = 'W'
+    board[2][1] = 'W'
+    board[2][2] = 'B'
+    board[3][0] = 'W'
+    board[3][3] = 'B'
+    return board
 
 # Define the function for making a move on the board
 
 
 def make_move(board, move, player):
-    # TODO: Implement the function for making a move
-    return board
+    col, row, direction = move[0], int(move[1]), move[3:]
+    new_board = [row[:] for row in board]
+    i = row - 1
+    j = ord(col) - ord('A')
+    new_board[i][j] = None
+    if direction == 'NW':
+        new_board[i-1][j-1] = player
+    elif direction == 'N':
+        new_board[i-1][j] = player
+    elif direction == 'NE':
+        new_board[i-1][j+1] = player
+    elif direction == 'W':
+        new_board[i][j-1] = player
+    elif direction == 'E':
+        new_board[i][j+1] = player
+    elif direction == 'SW':
+        new_board[i+1][j-1] = player
+    elif direction == 'S':
+        new_board[i+1][j] = player
+    elif direction == 'SE':
+        new_board[i+1][j+1] = player
+    return new_board
+
 
 # Define the function for checking if a player has won
-
-
 def check_win(board, player):
-    # TODO: Implement the function for checking if a player has won
+    # Check rows
+    for i in range(4):
+        if board[i] == [player, player, player, player]:
+            return True
+
+    # Check columns
+    for j in range(4):
+        if [board[i][j] for i in range(4)] == [player, player, player, player]:
+            return True
+
+    # Check corners
+    if board[0][0] == player and board[0][3] == player and board[3][0] == player and board[3][3] == player:
+        return True
+
+    # Check square
+    for i in range(3):
+        for j in range(3):
+            if board[i][j] == player and board[i][j+1] == player and board[i+1][j] == player and board[i+1][j+1] == player:
+                return True
+
     return False
+
 
 # Define the function for displaying the game board
 
@@ -52,12 +94,8 @@ def display_board(board):
 def get_user_move():
     while True:
         try:
-            move_str = input('Enter your move (e.g., C2 DR): ')
-            move_from, move_dir = move_str.split()
-            col = ord(move_from[0]) - ord('A')
-            row = int(move_from[1]) - 1
-            move = (row, col, move_dir)
-            return move
+            move_str = input('Enter your move (e.g., C2 SE): ')
+            return move_str
         except ValueError:
             print('Invalid move. Please try again.')
 
@@ -68,8 +106,9 @@ def get_user_move():
 def get_computer_move(state):
     global start_time
     start_time = time.time()
-    #move = alpha_beta_search(state, MAX_DEPTH)
+    move = alpha_beta_search(state, MAX_DEPTH)
     return move
+
 """
 
 
@@ -80,7 +119,7 @@ def get_computer_move(state):
     # Choose a random column, row and direction
     column = random.choice(columns)
     row = random.choice(rows)
-    direction = random.choice(['NW', 'NE', 'SW', 'SE'])
+    direction = random.choice(['N', 'S', 'E', 'W', 'NW', 'NE', 'SW', 'SE'])
     # Return the random move
     return f"{column}{row} {direction}"
 
@@ -88,7 +127,9 @@ def get_computer_move(state):
 # Define the function for playing the game
 
 
-def play_game(board):
+def play_game(state):
+
+    board = state[0]
 
     # Display the game board
     display_board(board)
@@ -109,13 +150,12 @@ def play_game(board):
         computer_player = BLACK
 
     player = random.choice([computer_player, human_player])
-
+    print(player)
     # Loop until the game is over
     while True:
         # Let the human player make a move
         if player == human_player:
             print('HUMAN TURN')
-
             move = get_user_move()
             board = make_move(board, move, player)
             display_board(board)
@@ -126,31 +166,29 @@ def play_game(board):
         else:
             print('AI TURN')
 
+            # state will helps us expand possible states in the future, right now it is not being used
             move = get_computer_move(state)
             board = make_move(board, move, player)
             display_board(board)
             if check_win(board, player):
-                print('Sorry, you lose!')
+                print('Sorry, you lose!, AI wins')
                 return
         # Switch the player
         player = WHITE if player == BLACK else BLACK
 
 
-# Define the game board as a 4x4 matrix of None values
-board = [[None for _ in range(4)] for _ in range(4)]
-# Define the player colors
-BLACK = 'B'
-WHITE = 'W'
+curent_player = None
 
-# Define the current player
-player = BLACK
-
-# Define the game state
-state = (board, player)
 
 # Define the maximum search depth for Minimax
 MAX_DEPTH = 3
 
 # Define the start time for measuring the computer's response time
 start_time = None
-play_game(board)
+
+
+# Define the game board as a 4x4 matrix of None values
+board = create_board()
+# Define the game state
+state = (board, curent_player)
+play_game(state)
