5973bdc03b5af4bd03ead1cc809ceb0183cbc4bc
Rodrigo Guardia <rodrigo.guardia316@gmail.com>
2023-03-31 16:59:36 -0400

    ME GANO ESTA COSA, VAMOS

--- game.py
+++ game.py
@@ -20,24 +20,7 @@ def create_board():
 
 # Define the function for making a move on the board
 
-def directions_to_move(direction):
-    return 0
-
-
-def make_move(board, move, player):
-    col = ord(move[0]) - ord('A')
-    row = int(move[1]) - 1
-    direction = move[3:]
-    
-    row_copy, col_copy = row, col
-    previous_position = [row_copy, col_copy]
-
-    if row_copy < 0 or row_copy > 3 or col_copy < 0 or col_copy > 3:
-        raise ValueError(
-            "Invalid move: position out of range ")
-    
-    new_board = [row[:] for row in board]
-
+def directions_to_move(direction, new_board, row_copy, col_copy, previous_position, player):
     if direction == 'NW' :
         for n in range(1, min(row_copy+1, col_copy+1)+1):
             if row_copy-n >= 0 and col_copy >= 0:
@@ -109,8 +92,97 @@ def make_move(board, move, player):
                 new_board[row_copy+n][col_copy+n] = player
                 new_board[previous_position[0]][previous_position[1]] = None
                 previous_position = [row_copy+n, col_copy+n]
-
     return new_board
+
+
+def make_move(board, move, player):
+    col = ord(move[0]) - ord('A')
+    row = int(move[1]) - 1
+    direction = move[3:]
+    
+    row_copy, col_copy = row, col
+    previous_position = [row_copy, col_copy]
+
+    if row_copy < 0 or row_copy > 3 or col_copy < 0 or col_copy > 3:
+        raise ValueError(
+            "Invalid move: position out of range ")
+    
+    new_board = [row[:] for row in board]
+
+    return directions_to_move(direction, new_board, row_copy, col_copy, previous_position, player)
+    # if direction == 'NW' :
+    #     for n in range(1, min(row_copy+1, col_copy+1)+1):
+    #         if row_copy-n >= 0 and col_copy >= 0:
+    #             if new_board[row_copy-n][col_copy-n] is not None:
+    #                 break
+    #             new_board[row_copy-n][col_copy-n] = player
+    #             new_board[previous_position[0]][previous_position[1]] = None
+    #             previous_position = [row_copy-n, col_copy-n]
+
+    # elif direction == 'N':
+    #     for n in range(1, row_copy+1):
+    #         if row_copy-n >= 0:
+    #             if new_board[row_copy-n][col_copy] is not None:
+    #                 break
+    #             new_board[row_copy-n][col_copy] = player
+    #             new_board[previous_position[0]][previous_position[1]] = None
+    #             previous_position = [row_copy-n, col_copy]
+
+    # elif direction == 'NE':
+    #     for n in range(1, min(row_copy+1, 4-col_copy)+1):
+    #         if row_copy-n >= 0 and col_copy+n < 4:
+    #             if new_board[row_copy-n][col_copy+n] is not None:
+    #                 break
+    #             new_board[row_copy-n][col_copy+n] = player
+    #             new_board[previous_position[0]][previous_position[1]] = None
+    #             previous_position = [row_copy-n, col_copy+n]
+
+    # elif direction == 'W':
+    #     for n in range(1, col_copy+1):
+    #         if col_copy-n >= 0:
+    #             if new_board[row_copy][col_copy-n] is not None:
+    #                 break
+    #             new_board[row_copy][col_copy-n] = player
+    #             new_board[previous_position[0]][previous_position[1]] = None
+    #             previous_position = [row_copy, col_copy-n]
+
+    # elif direction == 'E':
+    #     for n in range(1, 4-col_copy):
+    #         if col_copy+n < 4:
+    #             if new_board[row_copy][col_copy+n] is not None:
+    #                 break
+    #             new_board[row_copy][col_copy+n] = player
+    #             new_board[previous_position[0]][previous_position[1]] = None
+    #             previous_position = [row_copy, col_copy+n]
+
+    # elif direction == 'SW':
+    #     for n in range(1, min(4-row_copy, col_copy+1)+1):
+    #         if row_copy+n < 4 and col_copy-n > 0:
+    #             if new_board[row_copy+n][col_copy-n] is not None:
+    #                 break
+    #             new_board[row_copy+n][col_copy-n] = player
+    #             new_board[previous_position[0]][previous_position[1]] = None
+    #             previous_position = [row_copy+n, col_copy-n]
+
+    # elif direction == 'S':
+    #     for n in range(1, 4-row_copy):
+    #         if row_copy+n < 4:
+    #             if new_board[row_copy+n][col_copy] is not None:
+    #                 break
+    #             new_board[row_copy+n][col_copy] = player
+    #             new_board[previous_position[0]][previous_position[1]] = None
+    #             previous_position = [row_copy+n, col_copy]
+
+    # elif direction == 'SE':
+    #     for n in range(1, min(4-row_copy, 4-col_copy)+1):
+    #         if row_copy+n < 4 and col_copy+n < 4:
+    #             if new_board[row_copy+n][col_copy+n] is not None:
+    #                 break
+    #             new_board[row_copy+n][col_copy+n] = player
+    #             new_board[previous_position[0]][previous_position[1]] = None
+    #             previous_position = [row_copy+n, col_copy+n]
+
+    # return new_board
 
 
 # Define the function for checking if a player has won
@@ -173,6 +245,33 @@ def get_user_move():
 # #                 score += 1
 # #     return score
 
+def find_adjacencies(board):
+    number_b_adj = 0
+    number_w_adj = 0
+
+    for i in range(3):
+        for j in range(3):
+            if board[i][j] == BLACK:
+                if board[i+1][j] == BLACK:
+                    number_b_adj+=1
+                if board[i-1][j] == BLACK:
+                    number_b_adj+=1
+                if board[i][j+1] == BLACK:
+                    number_b_adj+=1
+                if board[i][j-1] == BLACK:
+                    number_b_adj+=1
+
+            elif board[i][j] == WHITE:
+                if board[i+1][j] == WHITE:
+                    number_w_adj+=1
+                if board[i-1][j] == WHITE:
+                    number_w_adj+=1
+                if board[i][j+1] == WHITE:
+                    number_w_adj+=1
+                if board[i][j-1] == WHITE:
+                    number_b_adj+=1
+    return number_b_adj, number_w_adj
+
 def evaluation_function(state):
     board = state[0]
     weights = [[3, 2, 2, 3],
@@ -189,7 +288,10 @@ def evaluation_function(state):
                 player1_score += weights[i][j]
             elif board[i][j] == WHITE:
                 player2_score += weights[i][j]
-    return player1_score - player2_score
+                
+    adj1, adj2 = find_adjacencies(board)
+    value = (player1_score-adj1) - (player2_score-adj2)
+    return value
     
 
 
