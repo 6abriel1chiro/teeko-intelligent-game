c8b9243e75e456b56a79a99dc6723556a04d3fd9
Angy Encinas <angy.encinas@ucb.edu.bo>
2023-03-30 21:39:39 -0400

    obtiene jugadas posibles

--- game.py
+++ game.py
@@ -7,20 +7,18 @@ WHITE = 'W'
 
 
 def create_board():
-    board = [[None for j in range(4)] for i in range(4)]
-    board[0][0] = 'B'
-    board[0][3] = 'W'
-    board[1][1] = 'B'
-    board[1][2] = 'W'
-    board[2][1] = 'W'
-    board[2][2] = 'B'
-    board[3][0] = 'W'
-    board[3][3] = 'B'
-    return board
+    return [['B', 'V', 'V', 'W'],
+            ['V', 'B', 'W', 'V'],
+            ['V', 'W', 'B', 'B'],
+            ['W', 'V', 'V', 'V']]
 
 # Define the function for making a move on the board
 
-
+def traducir(move):
+    rowt, col = int(move[0]), move[1]
+    row = rowt - 1
+    column = ord(col) - ord('A')
+    return row, column
 def make_move(board, move, player):
     col, row, direction = move[0], int(move[1]), move[3:]
     i = row - 1
@@ -29,7 +27,7 @@ def make_move(board, move, player):
         raise ValueError(
             "Invalid move: position out of range ")
     new_board = [row[:] for row in board]
-    new_board[i][j] = None
+    new_board[i][j] = 'V'
     if direction == 'NW':
         new_board[i-1][j-1] = player
     elif direction == 'N':
@@ -161,79 +159,138 @@ def utility(board):
     player2_score = calculate_score(board, -1)
     return player1_score - player2_score
 
-def is_legal_move(board, row, col):
-    if col < 0 or col >= len(board[0]):
+def is_possible_move(board, row, col, direction,color):
+    # Verificar que la posición dada sea válida en el tablero
+    if not (0 <= row < len(board) and 0 <= col < len(board[0])):
         return False
-    
-    # Verificar si la columna está llena
-    if board[0][col] != None:
+
+    # Verificar que la casilla en la que se encuentra la ficha a mover pertenezca al jugador actual
+    if board[row][col] != color:
+        
         return False
-    
-    # Verificar si la jugada no hace que la ficha se salga del tablero
-    for row in range(len(board)):
-        if board[row][col] == None:
-            return True
-    return False
-def possible_moves(board, player):
+
+    # Verificar que la dirección sea válida
+    if direction not in ['N', 'S', 'E', 'W', 'NW', 'NE', 'SW', 'SE']:
+        return False
+
+    # Verificar que el movimiento no lleve a una posición fuera del tablero
+    if (direction == 'N' and row == 0) or (direction == 'S' and row == len(board)-1) or \
+       (direction == 'W' and col == 0) or (direction == 'E' and col == len(board[0])-1):
+        return False
+    if (direction == 'NW' and (row == 0 or col == 0)) or \
+       (direction == 'NE' and (row == 0 or col == len(board[0])-1)) or \
+       (direction == 'SW' and (row == len(board)-1 or col == 0)) or \
+       (direction == 'SE' and (row == len(board)-1 or col == len(board[0])-1)):
+        return False
+
+    # Verificar que no haya otra ficha del mismo jugador en la casilla a la que se quiere mover
+    if direction == 'N' and board[row-1][col] == color:
+        return False
+    if direction == 'S' and board[row+1][col] == color:
+        return False
+    if direction == 'W' and board[row][col-1] == color:
+        return False
+    if direction == 'E' and board[row][col+1] == color:
+        return False
+    if direction == 'NW' and board[row-1][col-1] == color:
+        return False
+    if direction == 'NE' and board[row-1][col+1] == color:
+        return False
+    if direction == 'SW' and board[row+1][col-1] == color:
+        return False
+    if direction == 'SE' and board[row+1][col+1] == color:
+        return False
+
+    return True
+
+def possible_moves(board,color):
     moves = []
-    directions = ['N', 'S', 'E', 'W', 'NW', 'NE', 'SW', 'SE']
     for row in range(len(board)):
-        for col in range(len(board[row])):
-            if board[row][col] == None:
-                # La celda está vacía, se puede realizar un movimiento
-                    if is_legal_move(board, row, col):
-                    # El movimiento es legal, se agrega a la lista de movimientos
-                        for direction in directions:
-                            moves.append((row, col,direction))
+        for col in range(len(board[0])):
+            if board[row][col] == color:
+                for direction in ['N', 'S', 'E', 'W', 'NW', 'NE', 'SW', 'SE']:
+                    if is_possible_move(board, row, col, direction,color):
+                        moves.append((row, col, direction))
     return moves
 
-def result(board, move,player):
-    columns = ['A', 'B', 'C', 'D']
-    row = move[0]
-    columnchoose = move[1]
-    direction = move[2]
-    # Choose a random column, row and direction
-    column = columns[columnchoose]
-    # Return the random move
-    move2 = (row, column,direction)
-    return make_move(board,move2,player)
+def result(board,move,color):
+    i, j,direction= move[0],move[1],move[2]
+    new_board = [row[:] for row in board]
+    new_board[i][j] = 'V'
+    if direction == 'N':
+        new_board[i-1][j] = color
+    elif direction == 'S':
+        new_board[i+1][j] = color
+    elif direction == 'E':
+        new_board[i][j+1] = color
+    elif direction == 'W':
+        new_board[i][j-1] = color
+    elif direction == 'NW':
+        new_board[i-1][j-1] = color
+    elif direction == 'NE':
+        new_board[i-1][j+1] = color
+    elif direction == 'SW':
+        new_board[i+1][j-1] = color
+    elif direction == 'SE':
+        new_board[i+1][j+1] = color
+    return new_board
 
+def alpha_beta(board, depth, alpha, beta, maximizing_player,color):
+    if depth == 0 or terminal_test(board,color):
+        return None, utility(board)
+
+    if maximizing_player:
+        value = -math.inf
+        best_move = None
+        for move in possible_moves(board,color):
+            result_board = result(board, move,color)
+            _, temp_value = alpha_beta(result_board, depth-1, alpha, beta, False)
+            if temp_value > value:
+                value = temp_value
+                best_move = move
+            alpha = max(alpha, value)
+            if beta <= alpha:
+                break
+        return best_move, value
+    else:
+        value = math.inf
+        best_move = None
+        for move in possible_moves(board,color):
+            result_board = result(board, move,color)
+            _, temp_value = alpha_beta(result_board, depth-1, alpha, beta, True)
+            if temp_value < value:
+                value = temp_value
+                best_move = move
+            beta = min(beta, value)
+            if beta <= alpha:
+                break
+        return best_move, value
 
 # Define the function for playing the game
-def minimax(board, depth, maximizing_player, alpha, beta):
-    # Comprobar si se ha llegado al estado final o si se ha alcanzado el límite de profundidad
-    if depth == 0 or terminal_test(board, maximizing_player):
+def minmax(board, depth,color, utility, alpha, beta):
+    if depth == 0 or terminal_test(board,color):
         return utility(board)
-    
-    # Si el jugador es 'Max', elegir el movimiento que maximiza el resultado de minimax
-    if maximizing_player:
-        best_action = None
-        value = -math.inf
-        for move in possible_moves(board,maximizing_player):
-            result_board = result(board, move, maximizing_player)
-            _, new_value = minimax(result_board,depth-1,maximizing_player, alpha, beta)
-            if new_value > value:
-                value = new_value
-                best_action = move
-            alpha = max(alpha, value)
-            if alpha >= beta:
-                break
-        return best_action, value
-    
-    # Si el jugador es 'Min', elegir el movimiento que minimiza el resultado de minimax
-    else:
-        best_value = float('inf')
-        for move in possible_moves(board):
-            result_board = result(board, move, maximizing_player)
-            _, new_value = minimax(result_board,depth-1,maximizing_player, alpha, beta)
-            if new_value < value:
-                value = new_value
-                best_action = move
-            alpha = min(alpha, value)
-            if alpha >= beta:
-                break
-        return best_action, value
 
+    if color:
+        v = -math.inf
+        for move in possible_moves(board,color):
+            new_board = result(board, move,color)
+            print(new_board)
+            v = max(v, minmax(new_board, depth - 1, alpha, beta, False, utility))
+            alpha = max(alpha, v)
+            if beta <= alpha:
+                break  # poda alpha-beta
+        return v
+    else:
+        v = math.inf
+        for move in possible_moves(board,color):
+            new_board = result(board, move,color)
+            print(new_board)
+            v = min(v, minmax(new_board, depth - 1, alpha, beta, True, utility))
+            beta = min(beta, v)
+            if beta <= alpha:
+                break  # poda alpha-beta
+        return v
 def play_game(state):
 
     board = state[0]
@@ -283,55 +340,47 @@ def play_game(state):
         # Switch the player
         player = WHITE if player == BLACK else BLACK
 
-def play_game2(state,depth):
-
-    board = state[0]
-
-    # Display the game board
-    display_board(board)
-
-    # Let the user choose the color of the pieces
-    human_player = None
-    while human_player not in [BLACK, WHITE]:
-        try:
-            human_player = input(
-                "Choose your color ('B' for Black, 'W' for White): ").upper()
-        except ValueError:
-            print('Invalid input. Please try again.')
-
-    # Define the human player and the computer player
-    if human_player == BLACK:
-        computer_player = WHITE
+def play_game2(depth):
+# Definir el tablero
+    board = create_board()
+    ai_color = ''
+    # Definir el color del jugador humano
+    player_color = input("¿Qué color desea usar? (B/W) ").upper()
+    # Definir quién empieza a jugar
+    current_player = 'human' if player_color == 'B' else 'ai'
+    if player_color == 'B':
+        ai_color = 'W'
     else:
-        computer_player = BLACK
+        ai_color = 'B'
+    # Mientras el juego no haya terminado
+    while not terminal_test(board,current_player):
+        display_board(board)
 
-    player = random.choice([computer_player, human_player])
-    print(player)
-    # Loop until the game is over
-    while not terminal_test(state[0],player):
-        # Let the human player make a move
-        if player == human_player:
-            print('HUMAN TURN')
-            move = get_user_move()
-            board = make_move(board, move, player)
-            display_board(board)
-            if check_win(board, player):
-                print('Congratulations! You win!')
-                return
-        # Let the computer make a move
+        if current_player == 'human':
+        # Jugada del jugador humano
+            mov = input("Ingrese la fila y columna de la ficha que desea mover: ")
+            direction = input("Ingrese la dirección en la que desea mover la ficha (N, S, E, W, NW, NE, SW o SE): ")
+            row,col = traducir(mov)
+            move = [row,col,direction]
+            if is_possible_move(board,row, col, direction, player_color):
+                board = result(board, move, player_color)
+                current_player = 'ai'
+            else:
+                print("Jugada no válida. Inténtalo de nuevo.")
+
         else:
-            print('AI TURN')
+        # Jugada de la IA
+            alpha=-math.inf
+            beta=math.inf
+            #move = minmax(board,depth, ai_color,0,alpha,beta)
+            #print(move)
+            move = alpha_beta(board,depth,alpha,beta,ai_color)
+            board = result(board, move[0], move[1], move[2], 'W' if player_color == 'B' else 'B')
+            current_player = 'human'
 
-            # state will helps us expand possible states in the future, right now it is not being used
-            ai_action, _ = minimax(board,player, depth, alpha=-math.inf, beta=math.inf)
-            board = make_move(board,ai_action, player)
-            display_board(board)
-            if check_win(board, player):
-                print('Sorry, you lose!, AI wins')
-                return
-        # Switch the player
-        player = WHITE if player == BLACK else BLACK
-
+# Imprimir el resultado final del juego
+    display_board(board)
+    print("¡Fin del juego! El ganador es " + utility(board, player_color))
 curent_player = None
 
 
@@ -346,4 +395,4 @@ start_time = None
 board = create_board()
 # Define the game state
 state = (board, curent_player)
-play_game2(state,MAX_DEPTH)
+play_game2(MAX_DEPTH)
