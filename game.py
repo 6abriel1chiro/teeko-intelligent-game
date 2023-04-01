b597f13cb248cd8185e4e8f3f6d6f36f49f131d1
Rodrigo Guardia <rodrigo.guardia316@gmail.com>
2023-03-31 21:06:24 -0400

    ya es inteligentito

--- game.py
+++ game.py
@@ -1,7 +1,7 @@
 import time
 import random
 # from agent import *
-# Define the player colors
+# Define the player players
 BLACK = 'B'
 WHITE = 'W'
 
@@ -17,6 +17,13 @@ def create_board():
     board[3][0] = 'W'
     board[3][3] = 'B'
     return board
+
+
+def traduction_move(move):
+    col = ord(move[0]) - ord('A')
+    row = int(move[1]) - 1
+    direction = move[3:]
+    return row, col, direction
 
 # Define the function for making a move on the board
 
@@ -96,10 +103,7 @@ def directions_to_move(direction, new_board, row_copy, col_copy, previous_positi
 
 
 def make_move(board, move, player):
-    col = ord(move[0]) - ord('A')
-    row = int(move[1]) - 1
-    direction = move[3:]
-    
+    row, col, direction = traduction_move(move)
     row_copy, col_copy = row, col
     previous_position = [row_copy, col_copy]
 
@@ -162,6 +166,37 @@ def get_user_move():
         except ValueError:
             print('Invalid move. Please try again.')
 
+
+def is_possible_move(direction, row, col, board, player):
+    if (direction == 'N' and row == 0) or (direction == 'S' and row == len(board)-1) or \
+       (direction == 'W' and col == 0) or (direction == 'E' and col == len(board[0])-1):
+        return False
+    if (direction == 'NW' and (row == 0 or col == 0)) or \
+       (direction == 'NE' and (row == 0 or col == len(board[0])-1)) or \
+       (direction == 'SW' and (row == len(board)-1 or col == 0)) or \
+       (direction == 'SE' and (row == len(board)-1 or col == len(board[0])-1)):
+        return False
+
+    # Verificar que no haya otra ficha del mismo jugador en la casilla a la que se quiere mover
+    if direction == 'N' and (board[row-1][col] == player or board[row-1][col] == get_opponent(player)):
+        return False
+    if direction == 'S' and (board[row+1][col] == player or board[row+1][col] == get_opponent(player)):
+        return False
+    if direction == 'W' and (board[row][col-1] == player or board[row][col-1] == get_opponent(player)):
+        return False
+    if direction == 'E' and (board[row][col+1] == player or board[row][col+1] == get_opponent(player)):
+        return False
+    if direction == 'NW' and (board[row-1][col-1] == player or board[row-1][col-1] == get_opponent(player)):
+        return False
+    if direction == 'NE' and (board[row-1][col+1] == player or board[row-1][col+1] == get_opponent(player)):
+        return False
+    if direction == 'SW' and (board[row+1][col-1] == player or board[row+1][col-1] == get_opponent(player)):
+        return False
+    if direction == 'SE' and (board[row+1][col+1] == player or board[row+1][col+1] == get_opponent(player)):
+        return False
+
+    return True
+
 def find_adjacencies(board):
     number_b_adj = 0
     number_w_adj = 0
@@ -176,6 +211,14 @@ def find_adjacencies(board):
                 if board[i][j+1] == BLACK:
                     number_b_adj+=1
                 if board[i][j-1] == BLACK:
+                    number_b_adj+=1
+                if board[i+1][j-1] == BLACK:
+                    number_b_adj+=1
+                if board[i+1][j+1] == BLACK:
+                    number_b_adj+=1
+                if board[i-1][j+1] == BLACK:
+                    number_b_adj+=1
+                if board[i-1][j-1] == BLACK:
                     number_b_adj+=1
 
             elif board[i][j] == WHITE:
@@ -202,7 +245,7 @@ def evaluation_function(state):
     for i in range(4):
         for j in range(4):
             if board[i][j] == BLACK:
-                player1_score += weights[i][j]
+                player1_score += weights[i][j] 
             elif board[i][j] == WHITE:
                 player2_score += weights[i][j]
                 
@@ -230,12 +273,13 @@ def evaluation_function(state):
 #     value = player1_score - player2_score
 #     return value
 
-def AlphaBetaPrunningDepth(state, depth, alpha, beta, maximizing_player, available_moves):
+def AlphaBetaPrunningDepth(state, depth, alpha, beta, maximizing_player, available_moves, counter):
     board = state[0]
     player = state[1]
+    counter += 1
 
     if depth == 0 or (check_win(state[0], BLACK)>0) or (check_win(state[0], WHITE)>0) :
-        return evaluation_function(state), 0
+        return evaluation_function(state), 0,counter
     
     if maximizing_player:
         max_value = float('-inf')
@@ -243,15 +287,15 @@ def AlphaBetaPrunningDepth(state, depth, alpha, beta, maximizing_player, availab
         for move in available_moves:
             new_board = make_move(board, move, player)
             new_state = [new_board, get_opponent(player)]
-            value, _ = AlphaBetaPrunningDepth(new_state, depth-1, alpha,
-                               beta, False, available_moves)
+            value, _ ,counter= AlphaBetaPrunningDepth(new_state, depth-1, alpha,
+                               beta, False, available_moves,counter)
             if value > max_value:
                 max_value = value
                 best_move = move
             alpha = max(alpha, max_value)
             if beta <= alpha:
                 break
-        return max_value, best_move
+        return max_value, best_move,counter
 
     else:
         min_value = float('inf')
@@ -259,35 +303,41 @@ def AlphaBetaPrunningDepth(state, depth, alpha, beta, maximizing_player, availab
         for move in available_moves:
             new_board = make_move(board, move, player)
             new_state = [new_board, get_opponent(player)]
-            value, _ = AlphaBetaPrunningDepth(new_state, depth-1, alpha,
-                               beta, True, available_moves)
+            value, _ ,counter= AlphaBetaPrunningDepth(new_state, depth-1, alpha,
+                               beta, True, available_moves,counter)
             if value < min_value:
                 min_value = value
                 best_move = move
             beta = min(beta, min_value)
             if beta <= alpha:
                 break
-        return min_value, best_move
+        return min_value, best_move,counter
 
 
 def get_computer_move(state):
     board = state[0]
     player = state[1]
     available_moves = []
+    counter=0
+
 
     for i in range(4):
         for j in range(4):
             if board[i][j] == player:
                 for direction in ['N', 'S', 'E', 'W', 'NW', 'NE', 'SW', 'SE']:
+                   
                     move = f"{chr(ord('A') + j)}{i+1} {direction}"
-                    available_moves.append(move)
+                    row, col, direction = traduction_move(move)
+                    if (is_possible_move(direction, row, col, board, player)):
+                        available_moves.append(move)
 
     if not available_moves:
         return None
 
     max_depth = 3
-    _, best_move = AlphaBetaPrunningDepth(state, max_depth, float(
-        '-inf'), float('inf'), True, available_moves)
+    _, best_move,counter = AlphaBetaPrunningDepth(state, max_depth, float(
+        '-inf'), float('inf'), True, available_moves,counter)
+    print("Number of states expanded: ", counter)
     return best_move
 
 
